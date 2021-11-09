d52ac00941ddae62ca46010013f00df96fd4024e
SimSchwebel <simon.schwebel@ecam-strasbourg.eu>
2021-11-09 11:02:41 +0100

    Cours 4

--- Cours/Serie IV/Ex4_BusinessSchedule.cs
+++ Cours/Serie IV/Ex4_BusinessSchedule.cs
@@ -8,20 +8,75 @@ namespace Serie_IV
 {
     public class BusinessSchedule
     {
+        private SortedDictionary<DateTime, DateTime> _schedule;
+        private DateTime _debut;
+        private DateTime _fin;
+
+        public BusinessSchedule()
+        {
+            _schedule = new SortedDictionary<DateTime, DateTime>();
+            _debut = new DateTime(2021, 1, 1);
+            _fin = new DateTime(2021, 12, 31);
+        }
+
         public bool IsEmpty()
         {
-            //TODO
+
+            if (_schedule.Count == 0)
+            {
+                return true;
+            }
+
             return false;
         }
 
         public void SetRangeOfDates(DateTime begin, DateTime end)
         {
-            //TODO
+            //si emploi du temps est vide, on peut modifier, sinon renvoi d'une exception
+            if (!IsEmpty())
+            {
+                throw new Exception("Titi");
+            }
+            //TODO: Si dates invalides (= null ou debut > fin)
+            if (begin == null || end == null || begin >= end)
+            {
+                throw new ArgumentException("Toto");
+            }
+
+            _debut = begin;
+            _fin = end;
+
+            /*
+            if (!IsEmpty() || _reunion <= _debut || _reunion >= _fin)
+            {
+                try
+                {
+                    Console.WriteLine("La réunion n'est pas valide");
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("L'agenda n'est pas vide");
+
+                    throw new Exception();
+                }
+            }
+            */
+
         }
 
         private KeyValuePair<DateTime, DateTime> ClosestElements(DateTime beginMeeting)
         {
             //TODO
+            DateTime _datemin = DateTime.MinValue;
+            DateTime _datemax = DateTime.MaxValue;
+            DateTime _date;
+
+            foreach (var meeting in _schedule)
+            {
+
+            }
+
+
             return new KeyValuePair<DateTime, DateTime>();
         }
 
@@ -45,7 +100,22 @@ namespace Serie_IV
 
         public void DisplayMeetings()
         {
-            //TODO
+            Console.WriteLine($"Emploi du Temps : {_debut} - {_fin}");
+            Console.WriteLine("-----------------------------------");
+            int nbre = 1;
+            if (IsEmpty())
+            {
+                Console.WriteLine("Pas de réunions programmées.");
+            }
+            else
+            {
+                foreach (var meeting in _schedule)
+                {
+
+                    Console.WriteLine($"Réunion {nbre} : {meeting.Key} - {meeting.Value}");
+                    nbre++;
+                }
+            }
         }
     }
 }
--- Cours/Serie IV/Program.cs
+++ Cours/Serie IV/Program.cs
@@ -10,21 +10,35 @@ namespace Serie_IV
     {
         static void Main(string[] args)
         {
-
+            /*
             int numbers = 0123465789;
             string[] names = new string[] { "aaron", "abby", "abdul", "abe", "abel", "abiga" };
 
             PhoneBook pb = new PhoneBook();
             pb.AddPhoneNumber("0632359698", "Arthur");
+
             for (int i = 0; i < names.Length; i++)
             {
-                pb.AddPhoneNumber
-
+                pb.AddPhoneNumber("0" + numbers.ToString(), names[i]);
+                numbers++;
             }
+            pb.PhoneContact("0123456789");
+            pb.DeletePhoneNumber("0123456789");
+            pb.DisplayPhoneBook();
+            /**/
+            //Exo 4
+            BusinessSchedule bS = new BusinessSchedule();
+            try
+            {
+                bS.SetRangeOfDates(DateTime.Today, DateTime.Now);
+                //bS.SetRangeOfDates(DateTime.Now, DateTime.Today);
+                bS.DisplayMeetings();
+            }
+            catch (Exception e)
+            {
 
-
-
-            Console.WriteLine();
+                Console.WriteLine(e.Message);
+            };
 
             // Keep the console window open
             Console.WriteLine("----------------------");
@@ -32,4 +46,5 @@ namespace Serie_IV
             Console.ReadKey();
         }
     }
+
 }
